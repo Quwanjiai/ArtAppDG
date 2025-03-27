6b6b7d5264c73deb44f13937073b41780a568249
Quwanjiai <daqwanlol99@gmail.com>
2025-03-27 14:58:30 -0400

    this the firt commit for my ArtApp and ive have added a splash screen a and welcoming screen

---
+++ app/src/main/java/org/baltimorecityschools/artapp/SplashActvity.java
@@ -0,0 +1,38 @@
+package org.baltimorecityschools.artapp;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActvity extends AppCompatActivity {
+ImageView log,img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_actvity);
+        img = findViewById(R.id.img);
+        log = findViewById(R.id.log);
+
+        img.animate().translationY(-3500).setDuration(800).setStartDelay(2500);
+        log.animate().translationY(1400).setDuration(850).setStartDelay(2500);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActvity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        },3500);
+
+
+    }
+}
