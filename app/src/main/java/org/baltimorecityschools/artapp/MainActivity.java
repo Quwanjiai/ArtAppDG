921384c7e5cc76224e9a154dac928b6897de6ba9
Quwanjiai <daquanaplo@gmail.com>
2025-03-31 21:15:51 -0400

    added more to my code

--- app/src/main/java/org/baltimorecityschools/artapp/MainActivity.java
+++ app/src/main/java/org/baltimorecityschools/artapp/MainActivity.java
@@ -1,7 +1,11 @@
 package org.baltimorecityschools.artapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +15,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton startbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startbtn = findViewById(R.id.startBTN);
+
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
