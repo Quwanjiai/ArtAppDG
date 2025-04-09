6c024d02ac97e915f6a2b487cda95f3cf6554d50
Quwanjiai <daqwanlol99@gmail.com>
2025-04-09 14:48:52 -0400

    working on adding the movation word to change by a button and the ai soon

--- app/src/main/java/org/baltimorecityschools/artapp/HomeActivity.java
+++ app/src/main/java/org/baltimorecityschools/artapp/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,25 +14,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageButton findBT,hmBT,srBTN,ssBTN;
+    ImageButton ssBTN;
+    ImageButton findBT;
+    ImageButton hmBT;
+    ImageButton srBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        findBT = findViewById(R.id.findderhBTN);
-        hmBT = findViewById(R.id.homeBTN);
-        srBTN = findViewById(R.id.helpBTN);
+        findBT =  findViewById(R.id.findderhBTN);
+        hmBT =  findViewById(R.id.homeBTN);
+        srBTN =  findViewById(R.id.helpBTN);
         ssBTN = findViewById(R.id.settingBTN);
 
         ssBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(HomeActivity.this, SSettingsActivity.class);
+                Toast.makeText(HomeActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(HomeActivity.this,SSettingsActivity.class);
                 startActivity(intent);
-
             }
         });
 
